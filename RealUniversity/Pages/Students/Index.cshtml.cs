8fb0ba15a0785fd482daa92dc6ab212666a12e01
Drew Williams <WilliamsAg2@hendrix.edu>
2026-04-29 22:04:50 -0500

    4th restart

---
+++ RealUniversity/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RealUniversity.Data;
+using RealUniversity.Models;
+
+namespace RealUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RealUniversity.Data.SchoolContext _context;
+
+        public IndexModel(RealUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
