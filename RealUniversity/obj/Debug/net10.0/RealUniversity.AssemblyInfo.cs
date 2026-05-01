d063097b6502a44166ccc2851da746265b9c7d48
Drew Williams <WilliamsAg2@hendrix.edu>
2026-04-30 21:25:15 -0500

    index fixed

--- RealUniversity/obj/Debug/net10.0/RealUniversity.AssemblyInfo.cs
+++ RealUniversity/obj/Debug/net10.0/RealUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e133ad8796a74c24c19a59697f3319ffa061d4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000f277b23ee8427c6b1882112200327da0f4cd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
