7324ce0d4a111500c56a2ecfde7f5c5d041a0094
Drew Williams <WilliamsAg2@hendrix.edu>
2026-05-04 16:07:44 -0500

    Trying to merge

--- RealUniversity/obj/Debug/net10.0/RealUniversity.AssemblyInfo.cs
+++ RealUniversity/obj/Debug/net10.0/RealUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1d61a5784cfbf0d8386935aa03f1e37c12faaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a04627fd3bb2a3796aa7aa60c60995b92b2526e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
